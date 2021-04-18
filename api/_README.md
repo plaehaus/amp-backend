6f08325dcee868899caf4bfc2d8b36034814b291
Stephan Pitois <spitois@gmail.com>
2021-04-18 12:04:20 -0400

    Complete switch to Netlify from Vercel

--- api/_README.md
+++
@@ -1,3 +0,0 @@
-Vercel Serverless Functions
-
-TODO: deprecated. Delete `/api` folder. Replaced with `/functions`.
--- functions/upcomingProductions.js
+++ functions/upcomingProductions.js
@@ -3,8 +3,6 @@ const Redis = require("ioredis");
 exports.handler = async function (event, context) {
     const redis = new Redis(process.env.REDIS_URL);
     const result = await redis.get("upcomingProductions");
-    // const upcomingProductions = JSON.parse(result);
-    // res.setHeader('Cache-Control', 's-maxage=10, stale-while-revalidate');
     return {
         statusCode: 200,
         body: result
