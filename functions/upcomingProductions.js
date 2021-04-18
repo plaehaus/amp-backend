a7d947b259d238af3a3b5e56693ca86ba036d1c7
Stephan Pitois <spitois@gmail.com>
2021-04-18 11:54:29 -0400

    Convert upcomingProductions to Netlify function

--- functions/upcomingProductions.js
+++ functions/upcomingProductions.js
@@ -1,10 +1,13 @@
 const Redis = require("ioredis");
 
-module.exports = async (req, res) => {
+exports.handler = async function (event, context) {
     const redis = new Redis(process.env.REDIS_URL);
     await redis.get("upcomingProductions").then(function (result) {
         const upcomingProductions = JSON.parse(result);
-        res.setHeader('Cache-Control', 's-maxage=10, stale-while-revalidate');
-        res.json(upcomingProductions);
+        // res.setHeader('Cache-Control', 's-maxage=10, stale-while-revalidate');
+        return {
+            statusCode: 200,
+            body: JSON.stringify(upcomingProductions)
+        };
     });
 };
