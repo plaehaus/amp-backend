4ebd62d41b184e3b64f4c5ff67628761b92e1995
Stephan Pitois <spitois@gmail.com>
2021-04-18 17:09:55 -0400

    CORS

--- functions/upcomingProductions.js
+++ functions/upcomingProductions.js
@@ -5,6 +5,11 @@ exports.handler = async function (event, context) {
     const result = await redis.get("upcomingProductions");
     return {
         statusCode: 200,
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Methods': 'GET'
+        },
         body: result
     };
 };
