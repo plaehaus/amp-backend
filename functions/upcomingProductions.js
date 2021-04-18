f294113490a5d5809e0e49f2354778ffb06977a7
Stephan Pitois <spitois@gmail.com>
2021-04-18 12:00:17 -0400

    Fix upcomingProductions result format

--- functions/upcomingProductions.js
+++ functions/upcomingProductions.js
@@ -2,12 +2,11 @@ const Redis = require("ioredis");
 
 exports.handler = async function (event, context) {
     const redis = new Redis(process.env.REDIS_URL);
-    await redis.get("upcomingProductions").then(function (result) {
-        const upcomingProductions = JSON.parse(result);
-        // res.setHeader('Cache-Control', 's-maxage=10, stale-while-revalidate');
-        return {
-            statusCode: 200,
-            body: JSON.stringify(upcomingProductions)
-        };
-    });
+    const result = await redis.get("upcomingProductions");
+    // const upcomingProductions = JSON.parse(result);
+    // res.setHeader('Cache-Control', 's-maxage=10, stale-while-revalidate');
+    return {
+        statusCode: 200,
+        body: result
+    };
 };
